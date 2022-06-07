5756946bb27288ff2aefffc9d345d70f401770c5
CM <etchiedaniel1@gmail.com>
2022-06-07 22:58:35 +0100

    Initialized Chat Project
    Initialized chat project with chat functionality working

---
+++ mini_chatapp/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for mini_chatapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chatapp import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_chatapp.settings')
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    }
+)
